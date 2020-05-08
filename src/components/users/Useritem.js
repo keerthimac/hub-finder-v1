4cbbf366b498c9b231faf63efb4379b136cf8d04
bob <keerthi.maco@gmail.com>
2020-05-08 20:01:16 +0530

    Stage 01 Finished

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Useritem = (props) => {
-  const { login, avatar_url, html_url } = props.user;
+  const { login, avatar_url } = props.user;
 
   return (
     <div className="card text-center">
       <img src={avatar_url} className="round-img" alt="" style={{ width: '60px' }} />
       <h3>{login}</h3>
-      <a href={html_url} className="btn btn-dark btn-sm my-1">
+      <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
         More
-      </a>
+      </Link>
     </div>
   );
 };
